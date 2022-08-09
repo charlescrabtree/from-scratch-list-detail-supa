45d65b2f5187be1746051167c89c40e6dfb50670
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-08-09 14:04:28 -0700

    added some javascript

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // import functions and grab DOM elements
 import { getPokemans } from './gopeekychu.js';
-import { renderGodlessBeasts } from './renderOMG.js';
+import { renderPokemonCard } from './renderOMG.js';
 // let state
-
+const pokeballContainer = document.getElementById('pokeball-container')
 // set event listeners 
+window.addEventListener('load', async() => {
+    const pokemans = await getPokemans();
+
+    for (let pokemon of pokemans) {
+        const pokeEl = renderPokemonCard(pokemon);
+
+        pokeballContainer.append(pokeEl);
+    }
+
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
